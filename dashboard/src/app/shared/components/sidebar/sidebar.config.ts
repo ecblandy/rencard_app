f1a7290b048912c10abca366cf5055edadccdd4d
Vinícius Blandy <Viny19381@hotmail.com>
2026-09-17 20:19:02 -0300

    fix: adjustments

--- dashboard/src/app/shared/components/sidebar/sidebar.config.ts
+++ dashboard/src/app/shared/components/sidebar/sidebar.config.ts
@@ -3,33 +3,111 @@ import { UserRole } from '../../types/user-role';
 export interface SidebarLink {
   label: string;
   path: string;
-  icon?: string; // pode ser nome de ícone ou classe CSS
+  icon?: string;
 }
 
 export const SIDEBAR_LINKS: Record<UserRole, SidebarLink[]> = {
   admin: [
-    { label: 'Dashboard', path: '/admin/dashboard', icon: 'lucideLayoutDashboard' },
-    { label: 'Cupons', path: '/admin/coupons', icon: 'bootstrapTicketPerforated' },
-    { label: 'Cartões físicos', path: '/admin/physical', icon: 'monoCreditCard' },
-    { label: 'Clientes', path: '/admin/users-and-subscriptions', icon: 'lucideUser' },
-    { label: 'Parceiros', path: '/admin/partners', icon: 'remixSuitcase2Line' },
-    { label: 'Integrações', path: '/admin/integrations', icon: 'bootstrapLink' },
-    { label: 'Configurações', path: '/admin/settings', icon: 'lucideSettings' },
+    {
+      label: 'Dashboard',
+      path: '/app/admin/dashboard',
+      icon: 'lucideLayoutDashboard',
+    },
+    {
+      label: 'Cupons',
+      path: '/app/admin/coupons',
+      icon: 'bootstrapTicketPerforated',
+    },
+    {
+      label: 'Cartões físicos',
+      path: '/app/admin/physical',
+      icon: 'monoCreditCard',
+    },
+    {
+      label: 'Clientes',
+      path: '/app/admin/users-and-subscriptions',
+      icon: 'lucideUser',
+    },
+    {
+      label: 'Parceiros',
+      path: '/app/admin/partners',
+      icon: 'remixSuitcase2Line',
+    },
+    {
+      label: 'Integrações',
+      path: '/app/admin/integrations',
+      icon: 'bootstrapLink',
+    },
+    {
+      label: 'Configurações',
+      path: '/app/admin/settings',
+      icon: 'lucideSettings',
+    },
   ],
+
   affiliate: [
-    { label: 'Dashboard', path: '/affiliate/dashboard', icon: 'lucideLayoutDashboard' },
-    { label: 'Pagamentos', path: '/affiliate/payment', icon: 'monoCreditCard' },
-    { label: 'Minhas vendas', path: '/affiliate/sales', icon: 'monoCreditCard' },
-    { label: 'Cupons', path: '/affiliate/coupons', icon: 'monoCreditCard' },
-    { label: 'Configurações', path: '/affiliate/settings', icon: 'lucideSettings' },
+    {
+      label: 'Dashboard',
+      path: '/app/affiliate/dashboard',
+      icon: 'lucideLayoutDashboard',
+    },
+    {
+      label: 'Pagamentos',
+      path: '/app/affiliate/payment',
+      icon: 'monoCreditCard',
+    },
+    {
+      label: 'Minhas vendas',
+      path: '/app/affiliate/sales',
+      icon: 'monoCreditCard',
+    },
+    {
+      label: 'Cupons',
+      path: '/app/affiliate/coupons',
+      icon: 'monoCreditCard',
+    },
+    {
+      label: 'Configurações',
+      path: '/app/affiliate/settings',
+      icon: 'lucideSettings',
+    },
   ],
+
   client: [
-    { label: 'Dashboard', path: '/client/dashboard', icon: 'lucideLayoutDashboard' },
-    { label: 'Assinatura', path: '/client/billing/preview', icon: 'lucideCrown' },
-    { label: 'Perfil', path: '/client/profile', icon: 'lucideUser' },
-    { label: 'Códigos & Apps', path: '/client/apps', icon: 'lucideQrCode' },
-    { label: 'Configurações', path: '/client/settings', icon: 'lucideSettings' },
-    { label: 'Suporte', path: '/client/support', icon: 'bootstrapHeadset' },
-    { label: 'Entrega', path: '/client/delivery-information', icon: 'lucideTruck' },
+    {
+      label: 'Dashboard',
+      path: '/app/client/dashboard',
+      icon: 'lucideLayoutDashboard',
+    },
+    {
+      label: 'Assinatura',
+      path: '/app/client/billing/preview',
+      icon: 'lucideCrown',
+    },
+    {
+      label: 'Perfil',
+      path: '/app/client/profile',
+      icon: 'lucideUser',
+    },
+    {
+      label: 'Códigos & Apps',
+      path: '/app/client/apps',
+      icon: 'lucideQrCode',
+    },
+    {
+      label: 'Configurações',
+      path: '/app/client/settings',
+      icon: 'lucideSettings',
+    },
+    {
+      label: 'Suporte',
+      path: '/app/client/support',
+      icon: 'bootstrapHeadset',
+    },
+    {
+      label: 'Entrega',
+      path: '/app/client/delivery-information',
+      icon: 'lucideTruck',
+    },
   ],
 };
